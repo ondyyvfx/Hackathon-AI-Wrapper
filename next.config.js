0a40c210856910e011d32e994a6135a7e15c1ce4
Alikhan Altayev <160579504+ondyyvfx@users.noreply.github.com>
2025-04-16 20:55:47 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
   output: 'export',
+  
   images: {
     unoptimized: true,
   },
+
   basePath: "/Hackathon-AI-Wrapper",
+
+  ...(isProd && {
+  }),
 };
 
 module.exports = nextConfig;
